b55274f20d2bd8a6757e71a3df609e4a72d3e04d
Karl von Randow <karl@cactuslab.com>
2014-01-07 18:00:58 +1300

    Add alpha to screen filter

--- framework/Source/GPUImageScreenBlendFilter.m
+++ framework/Source/GPUImageScreenBlendFilter.m
@@ -13,8 +13,8 @@ NSString *const kGPUImageScreenBlendFragmentShaderString = SHADER_STRING
  {
      mediump vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
      mediump vec4 textureColor2 = texture2D(inputImageTexture2, textureCoordinate2);
-     mediump vec4 whiteColor = vec4(1.0);
-     gl_FragColor = whiteColor - ((whiteColor - textureColor2) * (whiteColor - textureColor));
+     mediump vec4 whiteColor = vec3(1.0);
+     gl_FragColor = vec4(whiteColor - ((whiteColor - textureColor2 * textureColor2.a) * (whiteColor - textureColor * textureColor.a)), max(textureColor.a, textureColor2.a));
  }
 );
 #else
